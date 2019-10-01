2bc2799168f028b48dc4850c6926f82ab8368149
Rony <ronymmoura@gmail.com>
2019-09-30 21:10:48 -0300

    Criando classe de data inicial e final

---
+++ Date/DatasInicialFinal.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Intech.Lib.Util.Date
+{
+    public class DatasInicialFinal
+    {
+        public DateTime DataInicial { get; set; }
+        public DateTime DataFinal { get; set; }
+    }
+}
